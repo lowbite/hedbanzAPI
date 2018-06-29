1e4442cc8be7128e0187310a0ab3f6b06db935d8
Bohdan <newbite@ukr.net>
2018-06-29 21:04:10 +0300

    no sockets

--- src/main/java/com/hedbanz/hedbanzAPI/SocketIOServerRunner.java
+++ src/main/java/com/hedbanz/hedbanzAPI/SocketIOServerRunner.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class SocketIOServerRunner implements CommandLineRunner {
     private SocketIOServer server;
 
