18c0abdb425b7f765cbb6c8913016c2054a191d5
Bohdan <newbite@ukr.net>
2025-07-08 22:05:43 +0300

    test commit

--- src/main/java/com/hedbanz/hedbanzAPI/HedbanzApiApplication.java
+++ src/main/java/com/hedbanz/hedbanzAPI/HedbanzApiApplication.java
@@ -1,15 +1,9 @@
 package com.hedbanz.hedbanzAPI;
 
-import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketConfig;
-import com.corundumstudio.socketio.SocketIOServer;
 import com.hedbanz.hedbanzAPI.converter.*;
-import com.hedbanz.hedbanzAPI.exception.SocketExceptionListener;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ConversionServiceFactoryBean;
